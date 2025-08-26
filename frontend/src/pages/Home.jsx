e7449643fe863fa66af9f126f9de491306f08f88
Darshil122 <darshildabhi2005@gmail.com>
2025-08-26 17:43:06 +0530

    Update files

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 // import axios from "axios";
 
 const Home = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const formSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center px-4">
-      <form className="bg-white p-7 rounded-md shadow-md w-full max-w-sm">
+      <form
+        className="bg-white p-7 rounded-md shadow-md w-full max-w-sm"
+        onSubmit={handleSubmit(formSubmit)}
+      >
+        {/* toggle button sign in && sign up */}
         <div className="flex justify-between mb-6">
           <button
             type="button"
             onClick={() => setIsLogin(true)}
-            className={`font-bold text-xl w-full rounded-md py-1 ${
+            className={`font-bold text-xl w-full rounded-md ${
               isLogin
-                ? "text-gray-800 bg-blue-500"
-                : "text-blue-500 border border-blue-500"
+                ? "text-gray-800 bg-blue-300 py-1"
+                : "text-gray-800 hover:border border-blue-300"
             }`}
           >
             Sign In
@@ -23,8 +38,8 @@ const Home = () => {
             onClick={() => setIsLogin(false)}
             className={`font-bold text-xl w-full rounded-md ${
               !isLogin
-                ? "text-gray-800 bg-blue-500"
-                : "text-blue-500 border border-blue-500"
+                ? "text-gray-800 bg-blue-300 py-1"
+                : "text-gray-800 hover:border border-blue-300"
             }`}
           >
             Sign Up
@@ -40,10 +55,13 @@ const Home = () => {
               placeholder="Enter Name"
               id="name"
               className="w-full border border-gray-300 p-2 focus:outline-none focus:ring-1 focus:ring-blue-500 rounded-md"
+              {...register("name", { required: "name is required" })}
             />
+            {errors.name && (
+              <strong className="text-red-500">{errors.name.message}</strong>
+            )}
           </div>
         )}
-
         <div className="mb-4">
           <label htmlFor="email" className="font-semibold text-gray-700">
             Email:
@@ -53,7 +71,11 @@ const Home = () => {
             placeholder="Enter email"
             id="email"
             className="w-full border border-gray-300 p-2 focus:outline-none focus:ring-1 focus:ring-blue-500 rounded-md"
+            {...register("email", { required: "email is required" })}
           />
+          {errors.email && (
+            <strong className="text-red-500">{errors.email.message}</strong>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="password" className="font-semibold text-gray-700">
@@ -64,7 +86,15 @@ const Home = () => {
             placeholder="Enter password"
             id="password"
             className="w-full border border-gray-300 p-2 focus:outline-none focus:ring-1 focus:ring-blue-500 rounded-md"
+            {...register("password", {
+              required: "password is required",
+              minLength: {
+                value: 5,
+                message: "password must be at least 5 character",
+              },
+            })}
           />
+          {errors.password && (<strong className="text-red-500">{errors.password.message}</strong>)}
         </div>
         {!isLogin && (
           <div className="mb-4">
@@ -78,7 +108,9 @@ const Home = () => {
             />
           </div>
         )}
-        <button className="w-full font-bold text-xl bg-blue-500 hover:bg-blue-400 p-1.5 rounded-md">{isLogin ? "Sign In": "Sign Up"}</button>
+        <button className="w-full font-bold text-xl bg-blue-500 hover:bg-blue-400 p-1.5 rounded-md">
+          {isLogin ? "Sign In" : "Sign Up"}
+        </button>
       </form>
     </div>
   );
