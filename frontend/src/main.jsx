edbd44ae97496161cd7403ee9d9c3c1ada9238e5
Darshil122 <darshildabhi2005@gmail.com>
2025-10-03 16:59:37 +0530

    Update small change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import ChatProvider from "./Context/ChatProvider";
 
 createRoot(document.getElementById("root")).render(
-  <ChatProvider>
-    <BrowserRouter>
+  <BrowserRouter>
+    <ChatProvider>
       <App />
-    </BrowserRouter>
-  </ChatProvider>
+    </ChatProvider>
+  </BrowserRouter>
 );
